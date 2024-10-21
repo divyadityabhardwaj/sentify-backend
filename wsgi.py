35e27bb6f0d397bd5371f32f1e62c5e410d7d481
Divyaditya Bhardwaj <121032560+divyadityabhardwaj@users.noreply.github.com>
2024-10-21 14:22:36 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from app import app  # Import the Flask app from your app.py file
+from app import app as application  # Rename app to application for Gunicorn
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
